169cb85675fb104ecd8c06d169567a9c7efd47db
Vaibhav <vaibhav.pandey0806@gmail.com>
2024-11-04 23:17:47 +0530

    add: unary flow for client

--- client/main.go
+++ client/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"log"
+
+	pb "github.com/vaibhav0806/go-grpc-demo/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NamesList{
+		Names: []string{"Akhil", "Vaibhav", "Alice", "Bob"},
+	}
+
+	//callSayHello(client)
+
+	callSayHelloServerStream(client, names)
+}
